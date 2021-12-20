db98511987459051394f3929694152cc87bf5065
llh1347 <68180007+llh1347@users.noreply.github.com>
2021-12-20 15:15:46 +0800

    Update dlb.py

--- dlb.py
+++ dlb.py
@@ -33,7 +33,7 @@ def push(msg):
 time0 = int(time0 * 1000)
 url = f"http://service.100bt.com/creditmall/mall/page.jsonp?callback=jQuery17205352076733403225_1637631202861&pageIndex=11&pageSize=1&orderBy=1&_={time0}"
 
-payload = {'pageIndex': '11',
+payload = {'pageIndex': '20',
            'pageSize': '1',
            'orderBy': '1'}
 files = [
